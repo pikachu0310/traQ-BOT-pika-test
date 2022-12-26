3de9de391c89fb26ef197a2042dc8d57d91e2ab7
teruki0310 <sunnon@i.softbank.jp>
2022-12-26 18:55:22 +0900

    EditMessageの実装

--- api/message.go
+++ api/message.go
@@ -8,17 +8,31 @@ import (
 	"github.com/traPtitech/go-traq"
 )
 
-func PostMessage(channelID string, content string) {
+func PostMessage(channelID string, content string) *traq.Message {
 
 	bot := util.GetBot()
 
-	_, _, err := bot.API().
+	message, _, err := bot.API().
 		MessageApi.
 		PostMessage(context.Background(), channelID).
 		PostMessageRequest(traq.PostMessageRequest{
 			Content: content,
 		}).
 		Execute()
+	if err != nil {
+		log.Println(err)
+	}
+	return message
+}
+
+func EditMessage(messageID string, content string) {
+
+	bot := util.GetBot()
+
+	_, err := bot.API().
+		MessageApi.EditMessage(context.Background(), messageID).PostMessageRequest(traq.PostMessageRequest{
+		Content: content,
+	}).Execute()
 	if err != nil {
 		log.Println(err)
 	}
