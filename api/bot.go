c64e294e9bb180bce4d472d9e7f33bbb1e91ad40
pikachu0310 <sunnon@i.softbank.jp>
2023-02-06 03:59:28 +0900

    API BotJoin BotLeave IsBotJoinedの実装

---
+++ api/bot.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"context"
+	"example-bot/util"
+	"github.com/traPtitech/go-traq"
+)
+
+const BotID = "28de7741-6c36-4c53-94e0-4ace05c221b5"
+
+func BotJoin(ChannelID string) error {
+	bot := util.GetBot()
+	_, err := bot.API().BotApi.LetBotJoinChannel(context.Background(), BotID).PostBotActionJoinRequest(traq.PostBotActionJoinRequest{ChannelId: ChannelID}).Execute()
+	return err
+}
+
+func BotLeave(ChannelID string) error {
+	bot := util.GetBot()
+	_, err := bot.API().BotApi.LetBotLeaveChannel(context.Background(), BotID).PostBotActionLeaveRequest(traq.PostBotActionLeaveRequest{ChannelId: ChannelID}).Execute()
+	return err
+}
+
+func IsBotJoined(ChannelID string) (bool, error) {
+	bot := util.GetBot()
+	bots, _, err := bot.API().BotApi.GetChannelBots(context.Background(), ChannelID).Execute()
+	if err != nil {
+		return false, err
+	}
+	for _, bot := range bots {
+		if bot.Id == BotID {
+			return true, nil
+		}
+	}
+	return false, nil
+}
