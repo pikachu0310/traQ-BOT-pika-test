8349ef35575b0506d0a0bd031eb22ba0396a992e
pikachu0310 <sunnon@i.softbank.jp>
2023-02-06 00:39:22 +0900

    API GetStampByStampName等の実装

--- api/stamp.go
+++ api/stamp.go
@@ -55,11 +55,50 @@ func GetStamp(stampID string) *traq.Stamp {
 	return stamp
 }
 
-func AddStamp(stamp traq.Stamp) {
+func GetStampWithErr(stampID string) (*traq.Stamp, error) {
+	fmt.Println("GetStamp", stampID)
+	bot := util.GetBot()
+	stamp, _, err := bot.API().StampApi.GetStamp(context.Background(), stampID).Execute()
+	return stamp, err
+}
+
+func AddStamp(messageID string, stamp traq.Stamp) {
 	fmt.Println("AddStamp", stamp)
 	bot := util.GetBot()
-	_, err := bot.API().StampApi.AddMessageStamp(context.Background(), stamp.Id, stamp.Id).Execute()
+	_, err := bot.API().StampApi.AddMessageStamp(context.Background(), messageID, stamp.Id).Execute()
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func AddStampByStampID(messageID string, stampID string) error {
+	fmt.Println("AddStamp", stampID)
+	stamp, err := GetStampWithErr(stampID)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	bot := util.GetBot()
+	_, err = bot.API().StampApi.AddMessageStamp(context.Background(), messageID, stamp.Id).Execute()
+	return err
+}
+
+func GetStampIdByStampName(stampName string) string {
+	stamps := GetAllStamps()
+	for _, stamp := range stamps {
+		if stamp.Name == stampName {
+			return stamp.Id
+		}
+	}
+	return ""
+}
+
+func GetStampByStampName(stampName string) (*traq.Stamp, error) {
+	stamps := GetAllStamps()
+	for _, stamp := range stamps {
+		if stamp.Name == stampName {
+			return &stamp, nil
+		}
+	}
+	return nil, fmt.Errorf("stamp not found")
 }
