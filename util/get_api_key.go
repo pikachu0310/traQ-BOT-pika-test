f3116bd728472f98802307832a088107d3a50b71
pikachu0310 <sunnon@i.softbank.jp>
2023-04-04 01:03:16 +0900

    コンピュータ内の環境変数で動くように

--- util/get_api_key.go
+++ util/get_api_key.go
@@ -1,17 +1,14 @@
 package util
 
 import (
-	"fmt"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GetApiKey() (apiKey string) {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf("error: tokenが読み込めなかった!: %v", err)
-	}
-	apiKey = os.Getenv("apiKey")
+	// 	err := godotenv.Load(".env")
+	// 	if err != nil {
+	// 		fmt.Printf("error: tokenが読み込めなかった!: %v", err)
+	// 	}
+	apiKey = os.Getenv("APIKEY")
 	return apiKey
 }
--- util/gettoken.go
+++ util/gettoken.go
@@ -1,17 +1,14 @@
 package util
 
 import (
-	"fmt"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GetToken() (token string) {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf("error: tokenが読み込めなかった!: %v", err)
-	}
-	token = os.Getenv("token")
+	//err := godotenv.Load(".env")
+	//if err != nil {
+	//	fmt.Printf("error: tokenが読み込めなかった!: %v", err)
+	//}
+	token = os.Getenv("TOKEN")
 	return token
 }
